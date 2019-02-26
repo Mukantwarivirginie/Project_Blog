1e287c44a1032df27c85c75c79570a01a08fac34
mukantwarivirginie <virgm2020@gmail.com>
2019-02-26 17:20:26 +0200

    correct some in css

--- app/main/views.py
+++ app/main/views.py
@@ -47,50 +47,50 @@ from .. import db,photos
 #         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@main.route('/movie/<int:movie_id>')
-def movie(movie_id):
+# @main.route('/movie/<int:movie_id>')
+# def movie(movie_id):
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    movie = get_movie(movie_id)
-    print(movie_id)
-    title = f'{movie.title}'
+#     '''
+#     View movie page function that returns the movie details page and its data
+#     '''
+#     movie = get_movie(movie_id)
+#     print(movie_id)
+#     title = f'{movie.title}'
 
-    reviews = Review.get_reviews(movie.id)
+#     reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+#     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
-@main.route('/search/<movie_name>')
-def search(movie_name):
-    '''
-    View function to display the search results
-    '''
-    movie_name_list = movie_name.split(" ")
-    movie_name_format = "+".join(movie_name_list)
-    searched_movies = search_movie(movie_name_format)
-    title = f'search results for {movie_name}'
-    return render_template('search.html',movies = searched_movies,title=title)
+# @main.route('/search/<movie_name>')
+# def search(movie_name):
+#     '''
+#     View function to display the search results
+#     '''
+#     movie_name_list = movie_name.split(" ")
+#     movie_name_format = "+".join(movie_name_list)
+#     searched_movies = search_movie(movie_name_format)
+#     title = f'search results for {movie_name}'
+#     return render_template('search.html',movies = searched_movies,title=title)
 
-@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-@login_required
-def new_review(id):
+# @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+# @login_required
+# def new_review(id):
 
-    form = ReviewForm()
+#     form = ReviewForm()
 
-    movie = get_movie(id)
+#     movie = get_movie(id)
 
-    if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
+#     if form.validate_on_submit():
+#         title = form.title.data
+#         review = form.review.data
 
-        new_review = Review(movie.id,title,movie.poster,review)
-        new_review.save_review()
+#         new_review = Review(movie.id,title,movie.poster,review)
+#         new_review.save_review()
 
-        return redirect(url_for('main.movie',movie_id = movie.id ))
+#         return redirect(url_for('main.movie',movie_id = movie.id ))
 
-    title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+#     title = f'{movie.title} review'
+#     return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
 
 @main.route('/user/<uname>')
