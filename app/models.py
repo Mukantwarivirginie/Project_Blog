2d3fd689b279c82780d0d3b95b6cec4a4940e2fd
mukantwarivirginie <virgm2020@gmail.com>
2019-02-26 16:09:47 +0200

    first commit

---
+++ app/models.py
@@ -0,0 +1,65 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitches',backref = 'pitches',lazy="dynamic")
+
+
+    @property
+    def password(self):
+       raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+      self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+     return check_password_hash(self.pass_secure,password)       
+
+    def __repr__(self):
+        return f'User {self.username}'
+       
+class Pitches(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    description = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    comments = db.relationship('Comments',backref = 'commen',lazy="dynamic")
+
+
+
+    def __repr__(self):
+        return f'Pitches {self.name}'       
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def __repr__(self):
+        return f'Pitches {self.name}'           
+
+
+
---
+++ start.sh
@@ -0,0 +1,8 @@
+export MOVIE_API_KEY=c6b1b929a62217f52f7ac0babce228f0
+export SECRET_KEY=459032567895562334
+
+
+
+export MAIL_USERNAME=virgm2020
+export MAIL_PASSWORD=iradukundak0
+python3.6 manage.py server 
